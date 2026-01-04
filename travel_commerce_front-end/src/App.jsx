4671029c2cbab0b703285840fb463026edc12900
Sandu70one <sanduaruna70one@gmail.com>
2026-01-04 08:55:17 +0530

    feat: add Contact page and integrate its route into the application.

--- travel_commerce_front-end/src/App.jsx
+++ travel_commerce_front-end/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 /* Pages */
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About"; // Imported About
+import Contact from "./pages/Contact/Contact"; // Imported Contact
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import AllServices from "./pages/Service/AllServices";
@@ -33,6 +34,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
       <Route path="/services" element={<AllServices />} />
       <Route path="/services/:id" element={<ServiceDetails />} />
       <Route path="/login" element={<Login />} />
